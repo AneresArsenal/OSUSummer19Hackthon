173bf91caeda78f04c3d49484cd0a6224fb6d73f
Serena Tay <tays@oregonstate.edu>
2019-06-21 16:01:03 -0700

    edits

--- js/main.js
+++ js/main.js
@@ -64,3 +64,19 @@ $BTN.click(function () {
   // Output the result
   $EXPORT.text(JSON.stringify(data));
 });
+
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
