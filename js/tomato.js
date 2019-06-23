69acb712ac2729e777be57d5a722e9a7ea60d4bd
Greg Healy <greg.healy@pm.me>
2019-06-23 16:01:38 -0400

    Made timer pausable

--- js/tomato.js
+++ js/tomato.js
@@ -1,7 +1,45 @@
+var countdown;
+var pauseActive = false;
+var pauseID;
+var countdownNumberEl = document.getElementById("countdown-number");
+
 $('.full-tomato').click((event) => {
   if ($('.full-tomato').hasClass('split')) {
-    $('.full-tomato').removeClass('split');
+    timeLeft = (countdownNumberEl.textContent).split(':');
+    countdown = Number(timeLeft[0] * 60) + Number(timeLeft[1]);
+    pauseTimer();
   } else {
-    $('.full-tomato').addClass('split');
+    pauseActive ? restartTimer() : startTimer();
   }
 })
+
+function startTimer() {
+  pauseActive = false;
+  $('.full-tomato').addClass('split');
+
+  //Set timer to 25 mins if not in the middle of a pause
+  if (!pauseActive) { var countdown = 25 * 60; }
+
+  countdownNumberEl.textContent = Math.floor(countdown/60) + ':' + (countdown%60).toString().padStart(2, '0');
+
+  pauseID = setInterval(function() {
+    countdown = --countdown <= 0 ? 0 : countdown;
+    countdownNumberEl.textContent = Math.floor(countdown/60) + ':' + (countdown%60).toString().padStart(2, '0');
+  }, 1000);
+}
+
+function pauseTimer() {
+  $('.full-tomato').removeClass('split');
+  pauseActive = true;
+  window.clearInterval(pauseID);
+}
+
+function restartTimer() {
+  console.log('restart at: ', countdown);
+  pauseActive = false;
+  $('.full-tomato').addClass('split');
+  pauseID = setInterval(function() {
+    countdown = --countdown <= 0 ? 0 : countdown;
+    countdownNumberEl.textContent = Math.floor(countdown/60) + ':' + (countdown%60).toString().padStart(2, '0');
+  }, 1000);
+}
